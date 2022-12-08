aedddcefa6f50a94ab2c79de3fc9331d0f5dffc4
tailang napa <tailangnapa1@gmail.com>
2022-12-09 00:36:59 +0530

    half middle done (font problem)

--- src/component/Card.js
+++ src/component/Card.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import"./Style.css"
+import React, { useState } from 'react';
+import "./Style.css"
 
 // importing swiper and tools
-import {Swiper,SwiperSlide} from 'swiper/react';
-import {FreeMode} from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper';
 import 'swiper/css';
 import "swiper/css/free-mode";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -11,13 +11,17 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //importing fontawsome
 // import {FontAwsomeIcon} from "@fortawesome/react-fontawesome";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {faLocationDot} from "@fortawesome/free-solid-svg-icons";
-import { faBuilding,faHouse,faBriefcase,faLandmark,faAd } from '@fortawesome/free-solid-svg-icons';
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import { faBuilding, faHouse, faBriefcase, faLandmark, faAd } from '@fortawesome/free-solid-svg-icons';
+import FontApi from './FontApi';
 
+let x = faBuilding;
 
 const Card = () => {
+    const [data,setData] = useState(FontApi);
     return (
         <>
+        
             <div className="container">
                 <div className="Upper">
                     <h3>Dream House:</h3>
@@ -26,56 +30,41 @@ const Card = () => {
                         <label htmlFor="/">Location</label>
                         <br />
                         <input type="text" placeholder='Everywhere' className='inp' />
-                        <div className="btn">icon</div>
+                        <div className="btn"><FontAwesomeIcon icon={faLocationDot}></FontAwesomeIcon></div>
                     </div>
                 </div>
                 <div className="middle">
                     <span id="category">Category</span>
                     <div className="Slider py-4 justify-content-center">
                         <Swiper
-                        freeMode={true}
-                        grabCursor={true}
-                        modules={[FreeMode]}
+                            freeMode={true}
+                            grabCursor={true}
+                            modules={[FreeMode]}
 
-                        className="mySwiper"
-                        slidesPerView={3}
-                        spaceBetween={30}
+                            className="mySwiper"
+                            slidesPerView={3}
+                            spaceBetween={30}
                         >
-                            <SwiperSlide className="middle-card">
-                                <FontAwesomeIcon icon={faHouse}
-                                className="fa-xl pt-2"></FontAwesomeIcon>
-                                <h6 className="x pt-2">House</h6>
-                            </SwiperSlide>
-                            <SwiperSlide className="middle-card">
-                                <FontAwesomeIcon icon={faHouse}
-                                className="fa-xl pt-2"></FontAwesomeIcon>
-                                <h6 className="x pt-2">House</h6>
-                            </SwiperSlide>
-                            <SwiperSlide className="middle-card">
-                                <FontAwesomeIcon icon={faHouse}
-                                className="fa-xl pt-2"></FontAwesomeIcon>
-                                <h6 className="x pt-2">House</h6>
-                            </SwiperSlide>
-                            <SwiperSlide className="middle-card">
-                                <FontAwesomeIcon icon={faHouse}
-                                className="fa-xl pt-2"></FontAwesomeIcon>
-                                <h6 className="x pt-2">House</h6>
-                            </SwiperSlide>
-                            <SwiperSlide className="middle-card">
-                                <FontAwesomeIcon icon={faHouse}
-                                className="fa-xl pt-2"></FontAwesomeIcon>
-                                <h6 className="x pt-2">House</h6>
-                            </SwiperSlide>
-                            <SwiperSlide className="middle-card">
-                                <FontAwesomeIcon icon={faHouse}
-                                className="fa-xl pt-2"></FontAwesomeIcon>
-                                <h6 className="x pt-2">House</h6>
-                            </SwiperSlide>
+                            {data.map((curElem) => {
+                                const { id,font,hero,name } = curElem;
+                                console.log(font)
+                                return (
+                                    <>
+                                        <SwiperSlide className="middle-card" key={curElem.id}>
+                                            <FontAwesomeIcon icon={faBuilding}
+                                                className="fa-xl pt-2"></FontAwesomeIcon>
+                                             
+                                            <h6 className="x pt-2">{name}</h6>
+                                        </SwiperSlide>
+                                    </>
+                                )
+                            })}
+                           
 
                         </Swiper>
                     </div>
                 </div>
-                    
+
                 <div className="lower">
                 </div>
             </div>
---
+++ src/component/FontApi.js
@@ -0,0 +1,39 @@
+const FontApi = [
+    {
+        id:1,
+        font:"faBuilding",
+        hero:"one",
+        name:"Building"
+    },
+    {
+         id:2,
+        font:"faHouse",
+        hero:"two",
+        name:"House"
+    },
+    {
+        id:3,
+        font:"faBuilding",
+        hero:"three",
+        name:"Room"
+    },
+    {
+        id:4,
+        font:"faBriefcase",
+        hero:"four",
+        name:"Briefcase"
+    },
+    {
+        id:5,
+        font:"faLandmark",
+        hero:"five",
+        name:"Landmark"
+    },
+    {
+        id:6,
+        font:"faAd",
+        hero:"six",
+        name:"Ad"
+    },
+]
+export default FontApi;
